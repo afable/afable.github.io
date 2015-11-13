23af1e0538171a0f023f9c13231b193b9324056b
afable <afablee@gmail.com>
2015-11-13 11:15:57 -0800

    basic css keyframes and transitions added. basic layout created. basic javascript/jquery functionality created. type definitions for jquery added for vscode project

--- js/main.js
+++ js/main.js
@@ -1,1 +1,42 @@
-console.log('This would be the main JS file.');
+var ae = {}; //closure namespace to avoid collisions with other js functions and keep variables out of the global window namespace
+
+(function () {
+	$("document").ready(function () {
+		console.log("jquery is a go-go.");
+	});
+
+	ae.init = function () {
+		console.log("rest of document loaded.");
+
+		/**
+		 * set buttons to active state when clicked.
+		 */
+		$("nav.top li").on("click.active", function () {
+			$("nav.top li").removeClass("active");
+			$(this).addClass("active");
+		});
+		
+		/**
+		 * nav.map clicks will toggle item aside info opacity. moving out of nav.map
+		 * will hide info opacity.
+		 */
+		$("nav.map").on("click.revealAside", function () {
+			( Number($("nav.map aside").css("opacity")) === 0 ) ? $("nav.map aside").fadeTo("slow", 1) : $("nav.map aside").fadeTo("slow", 0); 
+		});
+		
+		$("nav.map").on("mouseleave.hideAside", function () {
+			$("nav.map aside").fadeTo("slow", 0);
+		});
+	}
+
+
+
+
+
+
+
+
+
+
+	window.onload = ae.init;
+})();
