678f3822bb7aed10a677c342cc52a88809bfacab
afable <afablee@gmail.com>
2015-11-14 00:10:50 -0800

    added multiple main.home section/aside entries that toggle when nav.map view more details box clicked

--- js/main.js
+++ js/main.js
@@ -20,9 +20,10 @@ var ae = {}; //closure namespace to avoid collisions with other js functions and
 		console.log("rest of document loaded.");
 		
 		/**
-		 * hide all pages that are not the active page
+		 * hide all pages that are not active. hide all main.home section/aside info pages that are not active. 
 		 */
 		$("main:not(.active)").fadeOut("slow");
+		$("main.home > section:not(.active), main.home > aside:not(.active)").fadeOut("slow");
 
 		/**
 		 * set buttons to active state when clicked. load the correct page after button clicked.
@@ -35,32 +36,34 @@ var ae = {}; //closure namespace to avoid collisions with other js functions and
 				$("nav.top li").removeClass("active");
 				$(this).addClass("active");
 
-				$("main." + pageCurr + ", footer").removeClass("active").fadeOut("fast", function () {
-					$("main." + pageNext + ", footer").addClass("active").fadeIn("fast");
+				$("main." + pageCurr).removeClass("active").fadeOut("fast", function () {
+					$("main." + pageNext).addClass("active").fadeIn("fast");
 				});
 			}
 		});
 		
 		/**
-		 * nav.map clicks will toggle item aside info opacity. moving out of nav.map
-		 * will hide info opacity.
+		 * nav.map clicks will toggle item aside info opacity. clicks on nav.map aside when visible will load more detailed info below about target clicked. moving out of nav.map will hide info opacity.
 		 */
-		$("nav.map").on("click.revealAside", function () {
-			(Number($("nav.map aside").css("opacity")) === 0) ? $("nav.map aside").fadeTo("slow", 1) : $("nav.map aside").fadeTo("slow", 0);
+		$("nav.map").on("click.revealAside", function (event) {
+			// if we click on nav.map aside while it's visible then we want to view more info
+			if (event.target.nodeName === "ASIDE" && Number(event.target.style.opacity) === 1) {
+				// for now, get "date[\d+]" string and increment the digit to load next info page 
+				var infoCurr = $("main .active")[PAGE].className.split(" ")[PAGE];
+				var infoNext = infoCurr.replace(/[\d+]/i, function (n) { return (n%3)+1; });
+				$("main ." + infoCurr).removeClass("active").fadeOut("fast", function () {
+					$("main ." + infoNext).addClass("active").fadeIn("fast");
+				});
+			}
+			// nav.map clicks toggle nav.map aside opacity
+			(Number($("nav.map aside").css("opacity")) === 0) ? $("nav.map aside").fadeTo("fast", 1) : $("nav.map aside").fadeTo("slow", 0);
 		});
-
+		// leaving nav.map sets nav.map aside opacity to 0
 		$("nav.map").on("mouseleave.hideAside", function () {
 			$("nav.map aside").fadeTo("slow", 0);
 		});
 	}
-
-
-
-
-
-
-
-
+	
 
 
 	window.onload = ae.init;
