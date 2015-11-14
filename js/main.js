c5ee2a7862249f5454d2c637f8d82a8f0a2c300f
afable <afablee@gmail.com>
2015-11-13 22:05:04 -0800

    downloaded local jquery cdn. fixed nav.top animations and reveal animations. nav.top is now a flex. added hover border over nav.top tabs. nav.top tabs now properly select active tab and transition beautifully to active page. nav.map has been moved into main as a flex item to clearly identify as a main.home dom elt. added project and secret tabs/pages. basic jquery/js functionality added for movement between pages.

--- js/main.js
+++ js/main.js
@@ -1,19 +1,44 @@
 var ae = {}; //closure namespace to avoid collisions with other js functions and keep variables out of the global window namespace
 
 (function () {
+	/**
+	 * document constants
+	 */
+	var PAGE = 0;
+	
+	/**
+	 * jquery ready function
+	 */
 	$("document").ready(function () {
 		console.log("jquery is a go-go.");
 	});
 
+	/**
+	 * init function after document loaded
+	 */
 	ae.init = function () {
 		console.log("rest of document loaded.");
+		
+		/**
+		 * hide all pages that are not the active page
+		 */
+		$("main:not(.active)").fadeOut("slow");
 
 		/**
-		 * set buttons to active state when clicked.
+		 * set buttons to active state when clicked. load the correct page after button clicked.
 		 */
 		$("nav.top li").on("click.active", function () {
-			$("nav.top li").removeClass("active");
-			$(this).addClass("active");
+			var pageNext = $(this).attr("class").split(" ")[PAGE];
+			var pageCurr = $("main.active").attr("class").split(" ")[PAGE];
+
+			if (pageNext !== pageCurr) {
+				$("nav.top li").removeClass("active");
+				$(this).addClass("active");
+
+				$("main." + pageCurr + ", footer").removeClass("active").fadeOut("fast", function () {
+					$("main." + pageNext + ", footer").addClass("active").fadeIn("fast");
+				});
+			}
 		});
 		
 		/**
@@ -21,9 +46,9 @@ var ae = {}; //closure namespace to avoid collisions with other js functions and
 		 * will hide info opacity.
 		 */
 		$("nav.map").on("click.revealAside", function () {
-			( Number($("nav.map aside").css("opacity")) === 0 ) ? $("nav.map aside").fadeTo("slow", 1) : $("nav.map aside").fadeTo("slow", 0); 
+			(Number($("nav.map aside").css("opacity")) === 0) ? $("nav.map aside").fadeTo("slow", 1) : $("nav.map aside").fadeTo("slow", 0);
 		});
-		
+
 		$("nav.map").on("mouseleave.hideAside", function () {
 			$("nav.map aside").fadeTo("slow", 0);
 		});
