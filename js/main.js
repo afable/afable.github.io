f540d7f7726ed99b7d95a79a18d7659f7922f52c
afable <afablee@gmail.com>
2016-09-05 23:00:09 -0700

    index.html: #page-1 changed to #afable  as naming conventions will be used going forward with projects... .nav-button class added to buttons... data-tap-toggle='true' although it doesn't work on refresh of pages other than the main page...
    main.js: 'on' event handler used to bind on pagecreate to clone
    header and footer from #afable... manually added .ui-btn-active anchor
    class on pagecontainerchange... captured left & right swipes for
    future use...

--- js/main.js
+++ js/main.js
@@ -1,31 +1,71 @@
 (function() {
 
-	// bind an event handler to the "pagecreate" event for all "data-role='page'
-	// elements
+
+	// bind an event handler to the "pagecreate" event for all
+	// "data-role='page'" elements
 	var iPageCounter = 0;
-	$(document).delegate("[data-role='page']", "pagecreate", function () {
+	$(document).on("pagecreate", "[data-role='page']", function () {
 
-		// append and prepend header and footer divs to all newly created pages
-		// that are not #page-1
+		// prepend header and append footer divs to all newly created pages
+		// that are not the first page
 		++iPageCounter;
-		if ( $(this)[0].getAttribute("id") !== "page-1" ) {
-			// prepend header clone from #page-1
-			$("#page-1 > div.header").clone().prependTo(this);
-			console.log("cloning header for page " + iPageCounter);
+		if ( $(this)[0].getAttribute("id") !== "afable" ) {
+			console.log("cloning header & footer for " + $(this)[0].getAttribute("id"));
 
-			// prepend footer clone from #page-1
-			$("#page-1 > div.footer").clone().appendTo(this);
-			console.log("cloning footer for page" + iPageCounter);
+			// prepend header clone from #afable
+			$("#afable > div.header").clone(true).prependTo(this);
+
+			// append footer clone from #afable
+			$("#afable > div.footer").clone(true).appendTo(this);
 		}
-		console.log("creating page " + iPageCounter);
+		console.log(iPageCounter + " pages created...");
 	});
 
-	$(document).ready(function() {
-		console.log("$(document).ready...")
+
+	// change .ui-btn-active manually as it does not persist after transitions
+	// in jquerymobile-1.4.5 with jquery-2.2.4
+	$(document).on("pagecontainerchange", function() {
+		// remove any .ui-btn-active that exists in current page
+		var current = "#" + $(".ui-page-active").prop("id");
+		$(current + " a.ui-btn-active").removeClass("ui-btn-active");
+
+		// add .ui-btn-active to the current active page
+		$(current + " a[href='" + current + "']").addClass("ui-btn-active");
+
+		console.log("current page is " + current);
 	});
 
-	window.onload = function() {
-		console.log("window.onload...");
-	};
+
+	// capture left swipes on header, main, and footer
+	// left swipes move to next page in sequience
+	$(document).on("swipeleft", "[data-role='header']", function() {
+		console.log("swiped left on header");
+		var current = $(".ui-page-active").prop("id");
+		
+	});
+	$(document).on("swipeleft", "[role='main']", function() {
+		console.log("swiped left on main");
+	});
+	$(document).on("swipeleft", "[data-role='footer']", function() {
+		console.log("swiped left on footer");
+	});
+
+	// capture right swipes on header, main, and footer
+	// right swipes move to previous page in sequence
+	$(document).on("swiperight", "[data-role='header']", function() {
+		console.log("swiped right on header");
+	});
+	$(document).on("swiperight", "[role='main']", function() {
+		console.log("swiped right on main");
+	});
+	$(document).on("swiperight", "[data-role='footer']", function() {
+		console.log("swiped right on footer");
+	});
+
+
+
+	// document and window ready functions
+	// $(document).ready(function() { console.log("$(document).ready...") });
+	// window.onload = function() { console.log("window.onload..."); };
 	
 })();
