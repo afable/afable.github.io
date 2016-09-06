b2110f967c087ba252a9157e1da9048e03cc0074
afable <afablee@gmail.com>
2016-09-06 01:18:00 -0700

    index.html: pages are now created dynamically so cleared out all of body's elements... naming conventions changed from numbers to project names...
    main.js: moved JS LOAD function tests to top... passed JQuery as an
    argument to IIFE to scope $... pages created dynamically as soon as
    javascript loads... some refactoring for better naming conventions.

--- js/main.js
+++ js/main.js
@@ -1,38 +1,89 @@
-(function() {
+// document and window ready functions (test which loads first)
+window.onload = function() { console.log("LOADED window.onload..."); };
+(function() { console.log("LOADED IIFE.."); })();
+$(document).ready(function() { console.log("LOADED $(document).ready...") });
 
 
-	// bind an event handler to the "pagecreate" event for all
-	// "data-role='page'" elements
-	var iPageCounter = 0;
-	$(document).on("pagecreate", "[data-role='page']", function () {
+// encapsulate js so there are no naming conflicts
+(function($) {
 
-		// prepend header and append footer divs to all newly created pages
-		// that are not the first page
-		++iPageCounter;
-		if ( $(this)[0].getAttribute("id") !== "afable" ) {
-			console.log("cloning header & footer for " + $(this)[0].getAttribute("id"));
 
-			// prepend header clone from #afable
-			$("#afable > div.header").clone(true).prependTo(this);
+	// initialize page data for all pages
+	var arrPages = [];
+	arrPages.push( {id: "afable", data: "<p>Paragraph element for #afable.</p>"} );
+	arrPages.push( {id: "hamtarro", data: "<p>Because he likes cheese.</p>"} );
+	arrPages.push( {id: "slamsorrow", data: "<p>Young HARLEM.</p>"} );
 
-			// append footer clone from #afable
-			$("#afable > div.footer").clone(true).appendTo(this);
-		}
-		console.log(iPageCounter + " pages created...");
-	});
+
+	// create nav links first so they can be inserted into each page
+	var strNav = "";
+	var arrStrTransitions = ["fade", "flip", "pop", "flow", "slidefade", "slideup", "slidedown"];
+	for ( var i = 0; i < arrPages.length; ++i ) {
+		strNav += `<a href="#` + arrPages[i].id + `" data-role="button" data-transition="` + arrStrTransitions[i % arrStrTransitions.length] + `" class="nav-button ui-link ui-btn ui-shadow ui-corner-all" role="button">` + arrPages[i].id + `</a>`
+	}
+
+	// create all pages dynamically
+	for ( var i = 0; i < arrPages.length; ++i ) {
+		// header (from previously created strNav), main, and footer
+		var newPage = $(`
+			<div data-role=page id=` + arrPages[i].id + ` class=ui-page>
+				<div data-role="header" class="header ui-header ui-header-fullscreen ui-header-fixed slidedown ui-bar-inherit" data-position="fixed" data-fullscreen="true" data-tap-toggle="true" role="banner"> 
+					<nav id="nav">` + strNav + `</nav>
+				</div><!-- /header -->
+				<div role="main" class="main ui-content">
+					<section style="background-color: #fef; height: 111px;" class="content">
+						<h3>Some Title</h3>
+					</section>
+				</div><!-- /main -->
+				<div data-role="footer" class="footer ui-footer ui-footer-fullscreen ui-bar-inherit ui-footer-fixed slideup" data-position="fixed" data-fullscreen="true" data-tap-toggle="true">
+					<footer> 
+						<p>Written and coded by <a href="#">afable</a></p>
+					</footer>
+				</div><!-- /footer -->
+			</div>
+		`);
+
+		// prepend it to the page container
+		// newPage.prependTo( $.mobile.pageContainer );
+		newPage.prependTo( $("body") );
+		console.log("NEW PAGES CREATED");
+
+		
+	}
+
+
+	// // bind an event handler to the "pagecreate" event for all
+	// // "data-role='page'" elements
+	// var iPageCounter = 0;
+	// $(document).on("pagecreate", "[data-role='page']", function () {
+
+	// 	// prepend header and append footer divs to all newly created pages
+	// 	// that are not the first page
+	// 	++iPageCounter;
+	// 	if ( $(this)[0].getAttribute("id") !== "afable" ) {
+	// 		console.log("cloning header & footer for " + $(this)[0].getAttribute("id"));
+
+	// 		// prepend header clone from #afable
+	// 		$("#afable > div.header").clone(true).prependTo(this);
+
+	// 		// append footer clone from #afable
+	// 		$("#afable > div.footer").clone(true).appendTo(this);
+	// 	}
+	// 	console.log(iPageCounter + " pages created...");
+	// });
 
 
 	// change .ui-btn-active manually as it does not persist after transitions
 	// in jquerymobile-1.4.5 with jquery-2.2.4
 	$(document).on("pagecontainerchange", function() {
 		// remove any .ui-btn-active that exists in current page
-		var current = "#" + $(".ui-page-active").prop("id");
-		$(current + " a.ui-btn-active").removeClass("ui-btn-active");
+		var currPage = "#" + $(".ui-page-active").prop("id");
+		$(currPage + " a.ui-btn-active").removeClass("ui-btn-active");
 
 		// add .ui-btn-active to the current active page
-		$(current + " a[href='" + current + "']").addClass("ui-btn-active");
+		$(currPage + " a[href='" + currPage + "']").addClass("ui-btn-active");
 
-		console.log("current page is " + current);
+		console.log("currPage page is " + currPage);
 	});
 
 
@@ -40,7 +91,7 @@
 	// left swipes move to next page in sequience
 	$(document).on("swipeleft", "[data-role='header']", function() {
 		console.log("swiped left on header");
-		var current = $(".ui-page-active").prop("id");
+		var currPage = $(".ui-page-active").prop("id");
 		
 	});
 	$(document).on("swipeleft", "[role='main']", function() {
@@ -49,6 +100,7 @@
 	$(document).on("swipeleft", "[data-role='footer']", function() {
 		console.log("swiped left on footer");
 	});
+
 
 	// capture right swipes on header, main, and footer
 	// right swipes move to previous page in sequence
@@ -63,9 +115,4 @@
 	});
 
 
-
-	// document and window ready functions
-	// $(document).ready(function() { console.log("$(document).ready...") });
-	// window.onload = function() { console.log("window.onload..."); };
-	
-})();
+})(jQuery);
