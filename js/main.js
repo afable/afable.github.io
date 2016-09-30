45ed0b1f513308c7e001e6064098627af61aa3ff
afable <afablee@gmail.com>
2016-09-29 20:51:34 -0700

    added nojs handler to let users know javascript is required for this site.

--- js/main.js
+++ js/main.js
@@ -27,6 +27,9 @@ $(document).ready(function() { console.log("LOADED $(document).ready...") });
 // ============================================================================
 (function($) {
 
+	// hide nojs warning
+	$(".nojs").css("display", "none");
+
 	
 	// get if this viewport is hortizontal or vertical
 	g_bLandscape = ( window.innerWidth > window.innerHeight )? true : false;
